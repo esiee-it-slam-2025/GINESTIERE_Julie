033cbbcc636bad77a2b370fbcc4b48e4c9bd6d01
BuildTools <unconfigured@null.spigotmc.org>
2024-03-18 22:10:20 +0100

    Avancements textadventure

--- textadventure/textadventure.py
+++ textadventure/textadventure.py
@@ -10,8 +10,9 @@ try:
     OClef = json.load(open('OClef.json'))
     Salles = json.load(open('Salles.json'))
     Etages = json.load(open('Etages.json'))
+    Ennemis = json.load(open('Ennemis.json'))
 except:
-    print('\033[1;31mLes fichiers Oclef.json & Salles.json n''ont pas été trouvé.\nIl est conseillé de ne pas executer ce programme depuis VS code.\033[0;37m')
+    print('\033[1;31mLes fichiers Oclef.json & Salles.json n\'ont pas été trouvé.\nIl est conseillé de ne pas executer ce programme depuis VS code.\033[0;37m')
     quit()
 
 Code=""
@@ -19,13 +20,12 @@ Code=""
 
 personnage = hero(input('Comment t\'appelle tu?\n'), "a")#questionnaire()
 
-plats = "Ramen,Onigiri,Udon,Curry"  # À transformer en liste
 
-plats_stock = {}  # À remplir avec les plats
+plats_stock = {"Ramen":2,"Onigiri":2,"Udon":2,"Curry":2}  # À remplir avec les plats
 
 objets_cles = ["smartphone"]
 inventaire = {}
-
+breakpoint()
 # ********************************************************************************
 # FONCTIONS UTILITAIRES
 # ********************************************************************************
@@ -43,8 +43,14 @@ def UTText(Text):
     time.sleep(2)
     clearC()
 
-def fight():
-    print("a")
+def fight(Record):
+    lEnnemis = {}
+    for i, E in enumerate(Record):
+        lEnnemis[i] = {"Nom":E,"Attaque":Ennemis[E]["Attaque"]*Ennemis[E]["Multiplicateur"]*Record[E], "PV":Ennemis[E]["PV"]}
+
+    while personnage["PV"] > 0 | sum((lEnnemis.values)) > 0:
+        print("")
+
 
 def Action(Record):
     match Record["Type"]:
@@ -52,8 +58,15 @@ def Action(Record):
             UTText(Record["Texte"])
         case "fight":
             UTText(Record["Texte"])
-            fight(Record["Ennemi"])
-
+            fight(Record["Ennemis"])
+        case "NOClef":
+            if(Record["Objet"] not in objets_cles):
+                UTText(Record["Texte1"])
+                objets_cles.append(Record["Objet"])
+            else:
+                UTText(Record["Texte2"])
+        case "Unique":
+            eval(Record["Function"])
 
 def Escaliers(Etage):
     print(f"Vous êtes à l'étage {Etage}")
@@ -65,6 +78,14 @@ def Escaliers(Etage):
             return Etage
         except:
             print("Entrez une valeur entre 0 et 3")
+
+# ********************************************************************************
+# UNIQUE
+# ********************************************************************************
+def cantine():
+    print("Bonjour, voici les plats restants pour vous:")
+    for i, plat in enumerate(plats_stock):
+        print(f"{i+1} - {plat} (Reste {plats_stock[plat]})") if (plats_stock[plat] > 0) else False
 # ********************************************************************************
 # INTRODUCTION
 # ********************************************************************************
@@ -101,7 +122,7 @@ def lieu(Code):
         act+=1
     dep = act
     print(f'├──[Déplacements]')
-    for i, action in enumerate(Salles[Code]["Déplacements"]):
+    for i, action in enumerate(Salles[Code]["Deplacements"]):
         print(f'│   →{i+1+act}. {action["Nom"]}')
         dep += 1
     while True:
@@ -114,14 +135,14 @@ def lieu(Code):
             print(f"Erreur de saisie. Merci de saisir un entier entre 1 et {dep}")
     if select > act:
         clearC()
-        if(Salles[Code]["Déplacements"][select-1-act]["Key"] == None) | (Salles[Code]["Déplacements"][select-1-act]["Key"] in objets_cles):
-            return Salles[Code]["Déplacements"][select-1-act]["Code"]
-        print(f'Vous n\'avez pas accès à cette salle, il vous faut \"{Salles[Code]["Déplacements"][select-1-act]["Key"]}\"')            
+        if(Salles[Code]["Deplacements"][select-1-act]["Key"] == None) | (Salles[Code]["Deplacements"][select-1-act]["Key"] in objets_cles):
+            return Salles[Code]["Deplacements"][select-1-act]["Code"]
+        print(f'Vous n\'avez pas accès à cette salle, il vous faut \"{Salles[Code]["Deplacements"][select-1-act]["Key"]}\"')            
     else:
         Action(Salles[Code]["Actions"][select-1])
     
     return Code
-# ********************* ***********************************************************
+# ********************************************************************************
 # EXECUTION
 # ********************************************************************************
 
@@ -134,6 +155,7 @@ if __name__ == "__main__":
     Etage = 0
 
     while True:
+        clearC()
         if (Code == "E"):
             Etage = Escaliers(Etage)
             Code = Etages[Etage]["Hub"]
