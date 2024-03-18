93c6e51e93d93b2dd03a67eb55ab9b45dd7a46d2
julienginest <julien.ginestier+git@gmail.com>
2024-03-18 18:33:26 +0100

    TextAdventure

--- textadventure/textadventure.py
+++ textadventure/textadventure.py
@@ -1,20 +1,23 @@
 
-import os, json
+import os, json, time
 from Questionnaire import *
 
 
-
-
+OClef =None
+Salles = None
+Etages =None
 try:
     OClef = json.load(open('OClef.json'))
     Salles = json.load(open('Salles.json'))
+    Etages = json.load(open('Etages.json'))
 except:
     print('\033[1;31mLes fichiers Oclef.json & Salles.json n''ont pas été trouvé.\nIl est conseillé de ne pas executer ce programme depuis VS code.\033[0;37m')
     quit()
 
+Code=""
 
 
-personnage = hero(input('Comment t''appelle tu?\n'), "a")#questionnaire()
+personnage = hero(input('Comment t\'appelle tu?\n'), "a")#questionnaire()
 
 plats = "Ramen,Onigiri,Udon,Curry"  # À transformer en liste
 
@@ -22,7 +25,6 @@ plats_stock = {}  # À remplir avec les plats
 
 objets_cles = ["smartphone"]
 inventaire = {}
-Code = ""
 
 # ********************************************************************************
 # FONCTIONS UTILITAIRES
@@ -33,10 +35,36 @@ def clearC():
     else:
         _ = os.system('clear')
 
+def UTText(Text):
+    for i, l in enumerate(Text):
+        clearC()
+        print(Text[0:i+1])
+        time.sleep(0.01)
+    time.sleep(2)
+    clearC()
+
+def fight():
+    print("a")
 
 def Action(Record):
-    eval(Record["Type"]+'("' + Record["Action"] + '")')
+    match Record["Type"]:
+        case "print":
+            UTText(Record["Texte"])
+        case "fight":
+            UTText(Record["Texte"])
+            fight(Record["Ennemi"])
 
+
+def Escaliers(Etage):
+    print(f"Vous êtes à l'étage {Etage}")
+    while True:
+        try:
+            Etage = int(input("Vous allez à l'étage >"))
+            if Etage > 3 | Etage < 0:
+                raise Exception()
+            return Etage
+        except:
+            print("Entrez une valeur entre 0 et 3")
 # ********************************************************************************
 # INTRODUCTION
 # ********************************************************************************
@@ -57,13 +85,13 @@ def intro():
     # Stocker le nom du pouvoir choisi dans le dictionnaire "personnage"
 
     # Afficher tout le contenu (clé et valeur) du dictionnaire "personnage"
-    Code = "0-H"
-    lieu()
+    Code = '0-H'
+    return lieu(Code)
 
 # ********************************************************************************
 # LIEUX
 # ********************************************************************************
-def lieu():
+def lieu(Code):
     act = 0
     select=None
     print(f'│[{Salles[Code]["Nom"]}]')
@@ -86,9 +114,13 @@ def lieu():
             print(f"Erreur de saisie. Merci de saisir un entier entre 1 et {dep}")
     if select > act:
         clearC()
-        Code = Salles[Code]["Déplacements"][select-1-act]["Code"]
+        if(Salles[Code]["Déplacements"][select-1-act]["Key"] == None) | (Salles[Code]["Déplacements"][select-1-act]["Key"] in objets_cles):
+            return Salles[Code]["Déplacements"][select-1-act]["Code"]
+        print(f'Vous n\'avez pas accès à cette salle, il vous faut \"{Salles[Code]["Déplacements"][select-1-act]["Key"]}\"')            
     else:
         Action(Salles[Code]["Actions"][select-1])
+    
+    return Code
 # ********************* ***********************************************************
 # EXECUTION
 # ********************************************************************************
@@ -97,11 +129,14 @@ def lieu():
 # Pour lancer le jeu, on appelle la fonction d'introduction
 
 if __name__ == "__main__":
-    Code = "0-H"
-    intro()
+    Code = intro()
     
+    Etage = 0
+
     while True:
-        breakpoint()
-        lieu()
+        if (Code == "E"):
+            Etage = Escaliers(Etage)
+            Code = Etages[Etage]["Hub"]
+        Code = lieu(Code)
 
     print("Fin du jeu.")
