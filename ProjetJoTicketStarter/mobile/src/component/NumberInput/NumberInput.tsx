781012eef80a7634195e8de2c3b5d93d6c862db1
julieginest <git@j.ginestiere.fr>
2025-03-30 21:30:55 +0200

    feat(mobile): add NumberInput componentUsed to remove arrows from regular 'number' input

---
+++ ProjetJoTicketStarter/mobile/src/component/NumberInput/NumberInput.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+interface NumberInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const NumberInput: React.FC<NumberInputProps> = ({ className = "", value=0, ...props }) => {
+  const [nValue, setValue] = useState<string >(value.toString());
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value.replace(/\D/g, ''); // Retirer tout ce qui n'est pas un chiffre
+    setValue(newValue); // Mettre à jour l'état avec la valeur filtrée
+};
+
+  useEffect(()=>{},[nValue])
+
+
+  return (
+    <input
+      type="text"
+      value={nValue}
+      onInput={handleChange}
+      className={`${className}`}
+      {...props}
+    />
+  );
+};
+
+export default NumberInput;
+
