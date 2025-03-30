dd900871d1b0d63ab2b5fe67a8e6af9329f69233
julieginest <git@j.ginestiere.fr>
2025-03-30 21:27:07 +0200

    feat(mobile): add API parser for events

---
+++ ProjetJoTicketStarter/mobile/src/API/parseAPI.ts
@@ -0,0 +1,17 @@
+import {api} from "@/src/API/callAPI"
+import { Event } from "../interfaces/events"
+export function parseEvents({apiOutput}:{apiOutput: any[]}) : Event[]{
+    apiOutput.map(
+        val => ( {
+            id: val.id,
+            score: val.score,
+            start: new Date(val.score),
+            team_away: val.team_away,
+            team_home: val.team_home,
+            winner: val.winner,
+            stadium: val.stadium
+        })
+    )
+
+    return apiOutput as Event[]
+}
