6eb356194d615f1cb784c87f1b93209c8d4e2b84
julieginest <git@j.ginestiere.fr>
2025-04-07 15:02:43 +0200

    feat(admin): Adding previously added routes to urls.py

--- ProjetJoTicketStarter/admin/mainapp/urls.py
+++ ProjetJoTicketStarter/admin/mainapp/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from .views import (api,logout_view, AdminView, LoginFormView, StadiumView, TeamView, EventView, EventsEditView, TeamsEditView, StadiumsEditView)
+from .views import (api, validitySession, logout_view, AdminView, LoginFormView, LoginAPI, LogoutAPI, signInAPI, StadiumView, TeamView, EventView, EventsEditView, TeamsEditView, StadiumsEditView, login)
 from django.shortcuts import redirect
 
 # Un exemple de endpoint qui renverait les stades... Si la vue était faite :)
 urlpatterns = (
     path("", lambda request: redirect("admin/")),
     path("api/", api, name="api"),
+    path("api/login/", LoginAPI.as_view(), name="apiLogin"),
+    path("api/logout/", LogoutAPI.as_view(), name="apiLogout"),
+    path("api/signIn/", signInAPI.as_view(), name="apiSignIn"),
     path("api/stadiums/", StadiumView.as_view(), name="stadium"),
     path("api/teams/", TeamView.as_view(), name="team"),
     path("api/events/", EventView.as_view(), name="event"),
+    path("api/sessionCheck/", validitySession.as_view(), name="sessionCheck"),
 
     path("admin/", AdminView.as_view(), name="admin"),
     path("admin/login/", LoginFormView.as_view(), name="login"),
