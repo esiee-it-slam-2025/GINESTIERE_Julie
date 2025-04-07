37b4d9fbb4119c7a053de7f9f7255a34ede9f142
julieginest <git@j.ginestiere.fr>
2025-04-07 15:56:53 +0200

    style(mobile/logout): adding basic style to logout button

--- ProjetJoTicketStarter/mobile/app/compte/page.tsx
+++ ProjetJoTicketStarter/mobile/app/compte/page.tsx
@@ -1,8 +1,12 @@
 "use client"
+
+import styles from './page.module.css'
+
+
 import {GET} from "@/src/API/callAPI"
 export default function pages(){
     return <>
-        <button onClick={() => {GET("logout")}}>
+        <button className={styles.logoutButton} onClick={() => {GET("logout")}}>
             Déconnection
         </button>
     </>
